2573dfc01eb5f3e967e6183f2f4d26a948976ff1
grime3ja <grime3ja@dukes.jmu.edu>
2025-10-29 17:27:39 -0400

    and this one

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/https://github.com/grime3ja/personal-professional-profile/'
+  base: '/https://github.com/grime3ja/personal-professional-profile/'
 })
