32ff0497a0849dc2780eaad4089cabef83af5879
grime3ja <grime3ja@dukes.jmu.edu>
2025-11-12 15:53:16 -0500

    still trying

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
-// import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import LoadingScreen from './components/LoadingScreen'
 import Home from './pages/Home'
 import About from './pages/About'
 import Resume from './pages/Resume'
-import { HashRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 function App() {
   const [initialLoading, setInitialLoading] = useState(true)
@@ -30,24 +29,21 @@ function App() {
   }
 
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main
-          key={routeKey}
-          className="flex-grow container mx-auto px-4 py-8 transition-opacity duration-300"
-        >
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main
+        key={routeKey}
+        className="flex-grow container mx-auto px-4 py-8 transition-opacity duration-300"
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
